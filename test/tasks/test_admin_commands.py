dd687fff1338a90f47e4e6676010eeecfb0cd3b4
David Alexander <opensource@thelonelyghost.com>
2018-06-08 23:08:19 -0400

    Adds test support for admin commands

--- test/tasks/test_admin_commands.py
+++ test/tasks/test_admin_commands.py
@@ -1,12 +1,10 @@
-import pytest
-
 from tor.role_moderator.tasks import process_admin_command
 
 from ..celery import (
     signature,
     reset_signatures,
     # assert_no_tasks_called,
-    # assert_only_tasks_called,
+    assert_only_tasks_called,
 )
 from ..generators import (
     generate_message,
@@ -16,18 +14,26 @@ import unittest
 from unittest.mock import patch, MagicMock
 
 
-@pytest.mark.skip(reason='Not yet implemented')
+# @pytest.mark.skip(reason='Not yet implemented')
 class ProcessAdminCommandTest(unittest.TestCase):
     def setUp(self):
         reset_signatures()
         self.msg = generate_message()
 
-    # @patch('tor.role_moderator.tasks.signature', side_effect=signature)
-    # def test_import_tasks(self, mock_signature):
-    #     process_admin_command(subject='!ping',
-    #                           author='me',
-    #                           body='derp',
-    #                           message_id=self.msg.id)
+    @patch('tor.role_moderator.tasks.Config')
+    @patch('tor.role_moderator.tasks.signature', side_effect=signature)
+    def test_import_tasks(self, mock_signature, mock_config):
+        mock_config.subreddit = MagicMock(name='Config.subreddit',
+                                          return_value=mock_config)
+
+        process_admin_command(subject='!ping',
+                              author='me',
+                              body='derp',
+                              message_id=self.msg.id)
+        assert_only_tasks_called(
+            'tor.role_moderator.tasks.send_bot_message',
+        )
+        mock_config.commands.func.assert_called_once_with('ping')
 
     @patch('tor.role_moderator.tasks.Config')
     @patch('tor.role_moderator.tasks.signature', side_effect=signature)
@@ -39,3 +45,8 @@ class ProcessAdminCommandTest(unittest.TestCase):
                               author='me',
                               body='you',
                               message_id=self.msg.id)
+
+        assert_only_tasks_called(
+            'tor.role_moderator.tasks.send_bot_message',
+        )
+        mock_config.commands.func.assert_called_once_with('blacklist')
