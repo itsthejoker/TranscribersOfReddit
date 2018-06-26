2e0cd9c486cd2ce6e3956f4afcdb3c8929225655
David Alexander <opensource@thelonelyghost.com>
2018-06-26 01:50:00 -0400

    Adds config for testing celery worker in isolation

---
+++ celeryconfig.py
@@ -0,0 +1,17 @@
+from tor.celeryconfig import Config
+from tor_core import __BROKER_URL__
+from celery import Celery
+
+
+"""
+This is for testing purposes only. This file should not be used in production
+"""
+
+cfg = Config()
+cfg.beat_schedule = {}
+
+app = Celery('tor', broker=__BROKER_URL__)
+app.config_from_object(cfg)
+app.autodiscover_tasks(force=True, packages=[
+    'tor',
+])
