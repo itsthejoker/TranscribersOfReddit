9619c90bff80454240d912c6e6c83c96349833f0
David Alexander <opensource@thelonelyghost.com>
2019-06-17 23:34:38 -0400

    Use cached_property decorators to lazy load attributes

--- tor/core/config.py
+++ tor/core/config.py
@@ -2,6 +2,7 @@ import datetime
 import logging
 import os
 import random
+from typing import List
 
 from tor import __root__, __version__
 from tor.core import __HEARTBEAT_FILE__
@@ -60,7 +61,7 @@ class cached_property(object):
         return value
 
 
-class BaseConfig:
+class BaseConfig(object):
     """
     A base class used for all media-specific settings, e.g.,
     video, audio, image. This is intended to provide a unified
@@ -123,7 +124,7 @@ class OtherContentConfig(BaseConfig):
     """
 
 
-class Subreddit:
+class Subreddit(object):
     """
     Subreddit-specific configurations
 
@@ -151,58 +152,166 @@ class DefaultSubreddit(Subreddit):
     """
 
 
-class Config(object):
+class SubredditConfig(object):
+    """
+    A collection of Subreddit objects, injected later based on subreddit-specific rules
+    """
+
+    @cached_property
+    def subreddits(self) -> List[Subreddit]:
+        return []  # TODO
+
+    @cached_property
+    def subreddits_to_check(self) -> List[Subreddit]:
+        return []  # TODO
+
+    @cached_property
+    def subreddits_domain_filter_bypass(self) -> List[Subreddit]:
+        return []  # TODO
+
+    @cached_property
+    def upvote_filter_subs(self):
+        return {}  # TODO
+
+    @cached_property
+    def no_link_header_subs(self):
+        return []  # TODO
+
+
+class MediaConfig(object):
+    @cached_property
+    def video_formatting(self):
+        return ''  # TODO
+
+    @cached_property
+    def audio_formatting(self):
+        return ''  # TODO
+
+    @cached_property
+    def image_formatting(self):
+        return ''  # TODO
+
+    @cached_property
+    def video_domains(self):
+        return []  # TODO
+
+    @cached_property
+    def audio_domains(self):
+        return []  # TODO
+
+    @cached_property
+    def image_domains(self):
+        return []  # TODO
+
+    @cached_property
+    def media(self):
+        """
+        Media-specific rules, which are fetchable by a dict key. These
+        are intended to be programmatically accessible based on a
+        parameter given instead of hardcoding the media type in a
+        switch-case style of control structure
+        """
+        return {
+            'audio': AudioConfig(),
+            'video': VideoConfig(),
+            'image': ImageConfig(),
+            'other': OtherContentConfig(),
+        }
+
+
+class HomeSubredditInfo(object):
+    @cached_property
+    def mods(self):
+        """List of mods of ToR, fetched using PRAW"""
+        return []  # TODO
+
+    @cached_property
+    def tor_mods(self):
+        return []  # TODO
+
+    @cached_property
+    def header(self) -> str:
+        """
+        Templating string for the header of the bot post
+        """
+        return ''  # TODO
+
+    @cached_property
+    def tor(self):
+        if self.debug_mode:
+            return self.r.subreddit('ModsOfTor')
+        else:
+            return self.r.subreddit('transcribersofreddit')
+
+    @cached_property
+    def no_gifs(self):
+        return []  # TODO
+
+
+class ArchiveConfig(object):
+    @cached_property
+    def archive_time_default(self):
+        """
+        delay time for removing posts
+
+        (these are used by u/ToR_archivist)
+        """
+        return 0  # TODO
+
+    @cached_property
+    def archive_time_subreddits(self):
+        """
+        delay time for removing posts
+
+        (these are used by u/ToR_archivist)
+        """
+        return {}  # TODO
+
+
+class Config(MediaConfig, SubredditConfig, HomeSubredditInfo, ArchiveConfig):
     """
     A singleton object for checking global configuration from
     anywhere in the application
     """
 
-    # Media-specific rules, which are fetchable by a dict key. These
-    # are intended to be programmatically accessible based on a
-    # parameter given instead of hardcoding the media type in a
-    # switch-case style of control structure
-    media = {
-        'audio': AudioConfig(),
-        'video': VideoConfig(),
-        'image': ImageConfig(),
-        'other': OtherContentConfig(),
-    }
+    def __init__(self):
+        self.perform_header_check = True
+        self.debug_mode = False
 
-    # List of mods of ToR, fetched later using PRAW
-    mods = []
+        # this should get overwritten by the bot process
+        self.name = None
+        self.bot_version = __version__
 
-    # A collection of Subreddit objects, injected later based on
-    # subreddit-specific rules
-    subreddits = []
-    subreddits_to_check = []
-    subreddits_domain_filter_bypass = []
+        # Global flag to enable/disable placing the triggers for the OCR bot
+        self.OCR = True
+        self.heartbeat_logging = False
+        self.last_post_scan_time = datetime.datetime(1970, 1, 1, 1, 1, 1)
 
-    # API keys for later overwriting based on contents of filesystem
-    bugsnag_api_key = None
+    @cached_property
+    def bugsnag_api_key(self):
+        key = None
+        try:
+            with open('bugsnag.key', 'r') as f:
+                key = f.readlines().strip()
+                return key
+        except OSError:
+            key = os.getenv('BUGSNAG_API_KEY')
+            return key
+        finally:
+            # This is why it's important to set the value to `key`:
+            if bugsnag and key:
+                bugsnag.configure(
+                    api_key=key,
+                    app_version=__version__,
+                    project_root=__root__,
+                )
 
-    # Templating string for the header of the bot post
-    header = ''
-    modchat = None  # the actual modchat instance
-
-    no_gifs = []
-
-    perform_header_check = True
-    debug_mode = False
-
-    # delay times for removing posts; these are used by u/ToR_archivist
-    archive_time_default = None
-    archive_time_subreddits = {}
-
-    # Global flag to enable/disable placing the triggers
-    # for the OCR bot
-    OCR = True
-
-    # Name of the bot
-    name = None
-    bot_version = '0.0.0'  # this should get overwritten by the bot process
-    heartbeat_logging = False
-
-    last_post_scan_time = datetime.datetime(1970, 1, 1, 1, 1, 1)
+    @cached_property
+    def modchat(self):
+        """
+        Slack Client instance
+        """
+        return None  # TODO
 
     @cached_property
     def redis(self):
@@ -221,13 +330,6 @@ class Config(object):
             logging.fatal("Redis server is not running")
             raise
         return conn
-
-    @cached_property
-    def tor(self):
-        if self.debug_mode:
-            return self.r.subreddit('ModsOfTor')
-        else:
-            return self.r.subreddit('transcribersofreddit')
 
     @cached_property
     def heartbeat_port(self):
@@ -251,43 +353,5 @@ class Config(object):
                 return port
 
 
-try:
-    Config.bugsnag_api_key = open('bugsnag.key').readline().strip()
-except OSError:
-    Config.bugsnag_api_key = os.environ.get('BUGSNAG_API_KEY', None)
-
-if bugsnag and Config.bugsnag_api_key:
-    bugsnag.configure(
-        api_key=Config.bugsnag_api_key,
-        app_version=__version__,
-        project_root=__root__,
-    )
-
-
 # ----- Compatibility -----
 config = Config()
-config.core_version = __version__
-config.video_domains = []
-config.audio_domains = []
-config.image_domains = []
-
-config.video_formatting = ''
-config.audio_formatting = ''
-config.image_formatting = ''
-
-config.subreddits_to_check = []
-config.upvote_filter_subs = {}
-config.no_link_header_subs = []
-
-config.archive_time_default = 0
-config.archive_time_subreddits = {}
-
-config.tor_mods = []
-
-# section for gifs
-config.no_gifs = []
-
-# enables debug information for the cherrypy heartbeat server
-config.heartbeat_logging = False
-
-config.modchat = Config.modchat
